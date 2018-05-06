d01465ff60ad67bd8d3a4cfa13f0e36cffcd9449
hwlcode <hwlcode@163.com>
2018-05-06 22:56:21 +0800

    update

---
+++ build/common/conf.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var accessKeyId = 'LTAIEJ9h3PXQA2qk';
+exports.accessKeyId = accessKeyId;
+var secretAccessKey = 'UM6yx0sLnl5iADg7qEwxs4mjKSrBis';
+exports.secretAccessKey = secretAccessKey;
